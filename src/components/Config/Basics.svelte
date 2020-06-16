dd421ae69f49dd085bd2e17504a72dce59ddb63e
Benoit Pingris <benoit.pingris@epitech.eu>
2020-06-16 16:32:31 +0200

    hotfix

--- src/components/Config/Basics.svelte
+++ src/components/Config/Basics.svelte
@@ -21,7 +21,8 @@
     Photo:
     <input type="file" bind:files={photo} />
   </div>
-  <Input label="Name" bind:value={$user.name} />
+  <Input label="Firstname" bind:value={$user.firstname} />
+  <Input label="Lastname" bind:value={$user.lastname} />
   <Input label="Position" bind:value={$user.position} />
   <Input label="Description" type="textarea" bind:value={$user.description} />
 
